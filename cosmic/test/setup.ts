7b51461d3c5024e1f1955a919799cbe9b2368004
Hai Nguyen <quochai.kstn@gmail.com>
2024-01-03 00:30:00 +0700

    add setup/teardown docker compose script

---
+++ cosmic/test/setup.ts
@@ -0,0 +1,17 @@
+
+import { execSync } from 'child_process';
+
+// Function to start Docker Compose
+const startDockerCompose = () => {
+  try {
+    // start Docker Compose in detached mode, the files is in /devops/docker-compose.yml
+    execSync('docker-compose -f ./devops/docker-compose.yaml up -d', { stdio: 'inherit' });
+    console.log('Docker Compose started successfully.');
+  } catch (error) {
+    console.error('Failed to start Docker Compose:', error);
+    process.exit(1);
+  }
+};
+
+// Start Docker Compose before running tests
+export default startDockerCompose;
---
+++ cosmic/test/teardown.ts
@@ -0,0 +1,17 @@
+
+import { execSync } from 'child_process';
+
+
+// Function to stop Docker Compose
+const stopDockerCompose = () => {
+  try {
+    execSync('docker-compose -f ./devops/docker-compose.yaml down', { stdio: 'inherit' });
+    console.log('Docker Compose stopped successfully.');
+  } catch (error) {
+    console.error('Failed to stop Docker Compose:', error);
+    process.exit(1);
+  }
+};
+
+// Start Docker Compose before running tests
+export default stopDockerCompose;
