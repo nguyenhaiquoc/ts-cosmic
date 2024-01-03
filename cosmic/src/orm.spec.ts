29dee7e2e4c68f33e57e5dfb995ae63c95e7f320
Hai Nguyen <quochai.kstn@gmail.com>
2024-01-04 00:15:10 +0700

    add orm test

--- cosmic/src/orm.spec.ts
+++ cosmic/src/orm.spec.ts
@@ -1,8 +1,28 @@
+import { AppDataSource } from './orm';
+
+beforeAll(async () => {
+  await AppDataSource.initialize();
+});
+
 describe('AppDataSource', () => {
   it('should be available by issuing SELECT 1', async () => {
-    await AppDataSource.initialize();
     await AppDataSource.query('SELECT 1');
-    await AppDataSource.destroy();
   });
 });
-import { AppDataSource } from './orm';
+
+describe('check if table exist', () => {
+  // use test table technique to check if bacth, orderline, allocation table exist
+  it('check if table exist', async () => {
+    const tablename = ['batch', 'order_line', 'allocation'];
+    for (let i = 0; i < tablename.length; i++) {
+      const result = await AppDataSource.query(
+        `SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '${tablename[i]}');`,
+      );
+      expect(result[0].exists).toBe(true);
+    }
+  });
+});
+
+afterAll(async () => {
+  await AppDataSource.destroy();
+});
--- cosmic/src/orm.ts
+++ cosmic/src/orm.ts
@@ -1,4 +1,52 @@
-import { DataSource } from 'typeorm';
+import { Column, DataSource, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity } from 'typeorm';
+
+/* 
+  TypeOrm: declare Batch and OrderLine as , using Allocation as a join table
+  Batch has a one-to-many relationship with OrderLine
+  OrderLine has a many-to-one relationship with Batch
+*/
+@Entity()
+export class Batch {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  ref: string;
+
+  @Column()
+  sku: string;
+
+  @Column()
+  quantity: number;
+}
+
+@Entity()
+export class OrderLine {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  sku: string;
+
+  @Column()
+  quantity: number;
+}
+
+@Entity()
+export class Allocation {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  createdAt: Date;
+
+  @OneToMany(() => Batch, (batch) => batch.id)
+  batch: Batch;
+
+  @OneToMany(() => OrderLine, (orderLine) => orderLine.id)
+  orderLine: OrderLine;
+}
 
 // define a postgresql data source
 const AppDataSource = new DataSource({
@@ -8,6 +56,8 @@ const AppDataSource = new DataSource({
   username: 'test',
   password: 'test',
   database: 'test',
+  synchronize: true,
+  entities: [Batch, OrderLine, Allocation],
 });
 
 export { AppDataSource };
